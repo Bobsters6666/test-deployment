58a6a2b5f354ea1e43991155ea283542f8b62cae
Jason <79180247+Bobsters6666@users.noreply.github.com>
2023-05-17 19:55:33 +1200

    update url

--- gym-app/vite.config.js
+++ gym-app/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:4000",
+        target: "https://gym-app-4y5d.onrender.com/",
       },
     },
   },
